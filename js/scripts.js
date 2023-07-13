a0ac227707c37a19dcce74fbd1182bddc6037fb7
vanderleik <vanderleik@yahoo.com.br>
2023-07-13 18:17:48 -0300

    Criando a classe Validator e chamando o método validate

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,21 @@
+class Validator {
+
+    constructor() {
+        this.validations = [
+
+        ];
+    }
+}
+
 let form = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
+
+let validator = new Validator();
 
 //evento que dispara as validações
 submit.addEventListener("click", function(e) {
 
     e.preventDefault();
 
-    console.log("funcionou");
+    validator.validate(form);
 })
