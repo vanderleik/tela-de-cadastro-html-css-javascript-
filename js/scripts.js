ac1363cb93d2e5159f7dd9272a1913b0d9506492
vanderleik <vanderleik@yahoo.com.br>
2023-07-13 18:29:00 -0300

    Criando a estrutura inicial das validações

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,23 @@ class Validator {
 
         ];
     }
+
+    //iniciar a validação de todos os campos
+    validate(form) {
+
+        //pegar os inputs
+        let inputs = form.getElementsByTagName("input");
+        
+        //transformo uma HTMLCollection -> array
+        let inputsArray = [...inputs];
+
+        //loops nos inputs e validação mediante ao que for encontrado
+        inputsArray.forEach(function(input) {
+            console.log(input);
+
+        });
+
+    };
 }
 
 let form = document.getElementById("register-form");
@@ -18,4 +35,4 @@ submit.addEventListener("click", function(e) {
     e.preventDefault();
 
     validator.validate(form);
-})
+});
