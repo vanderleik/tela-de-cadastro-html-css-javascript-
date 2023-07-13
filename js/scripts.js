1487d370458753fca793c1f885a4f776a830e172
vanderleik <vanderleik@yahoo.com.br>
2023-07-13 18:14:59 -0300

    Iniciando a criação de um evento que dispara validações

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+let form = document.getElementById("register-form");
+let submit = document.getElementById("btn-submit");
+
+//evento que dispara as validações
+submit.addEventListener("click", function(e) {
+
+    e.preventDefault();
+
+    console.log("funcionou");
+})
